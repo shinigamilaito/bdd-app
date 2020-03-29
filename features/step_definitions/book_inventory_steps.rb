e36771c1cda0aa351cf95cd7a86792ee6e9502a0
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-29 11:33:23 -0600

    Generate book scaffold

--- features/step_definitions/book_inventory_steps.rb
+++ features/step_definitions/book_inventory_steps.rb
@@ -1,16 +1,3 @@
-Given("I am a registered user") do
-  @registered_user = FactoryBot.create(:user, email: "tester@testdomain.test", password: "pa$$word")
-end
-
-Given("I am logged in") do
-  visit root_path
-
-  fill_in "user_email", with: "tester@testdomain.test"
-  fill_in "user_password", with: "pa$$word"
-
-  click_button "Log in"
-end
-
 Given("I have populated my inventory with several books") do
   FactoryBot.create(:book,
                     user: @registered_user,
@@ -22,10 +9,6 @@ Given("I have populated my inventory with several books") do
                     name: "Moby Dick",
                     author: "Herman Melville")
 
-end
-
-When("I visit the homepage") do
-  visit root_path
 end
 
 Then("I should see the list of my books") do

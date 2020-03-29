a6a5900f0bb6ed32775c1e68ed778b862711bb64
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2020-03-29 13:38:38 -0600

    Add books to the inventory

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -22,4 +22,48 @@ RSpec.describe BooksController do
       expect(assigns(:books)).to eq(books)
     end
   end
+
+  describe "POST #create" do
+    let(:book) { FactoryBot.build_stubbed(:book) }
+    let(:params) { { name: "Moby-Dick", author: "Herman Melville" } }
+
+    before do
+      allow(book).to receive(:save)
+      allow(user).to receive_message_chain(:books, :build).and_return(book)
+    end
+
+    it "saves the book" do
+      post :create, params: { book: params }
+
+      expect(book).to have_received(:save)
+    end
+
+    context "when the book is succesfully saved" do
+      before do
+        allow(book).to receive(:save).and_return(true)
+
+        post :create, params: { book: params }
+      end
+
+      it "redirects to the book show page" do
+        expect(response).to redirect_to(book_path(book))
+      end
+
+      it "redirects to the book show page" do
+        expect(flash[:notice]).to eq("Book was successfully created.")
+      end
+    end
+
+    context "when the book can't be saved" do
+      before do
+        allow(book).to receive(:save).and_return(false)
+
+        post :create, params: { book: params }
+      end
+
+      it "redirects back to the new page" do
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
